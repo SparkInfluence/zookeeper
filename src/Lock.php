91dbb7f3c601cf5cd3d9dd3f535efa8e5a9586e1
John P. Bloch <johnpbloch@users.noreply.github.com>
2019-08-16 17:10:19 -0500

    Add locking

--- src/Lock.php
+++ src/Lock.php
@@ -2,11 +2,18 @@
 
 namespace SparkInfluence\Zookeeper;
 
+use SparkInfluence\Zookeeper\Exception\Exception;
+use Zookeeper as ZkExt;
+
 class Lock
 {
 
     /** @var ZookeeperInterface */
     private $zk;
+
+    const TYPE_EXCLUSIVE = 'exclusive';
+    const TYPE_READ = 'read';
+    const TYPE_WRITE = 'write';
 
     /**
      * Lock constructor.
@@ -15,6 +22,123 @@ class Lock
     public function __construct(ZookeeperInterface $zk)
     {
         $this->zk = $zk;
+    }
+
+    public function lock(string $key, int $timeout = 0): ?string
+    {
+        try {
+            $full_key = $this->getLockName($key);
+            $lock_key = $this->createLockKey($full_key);
+
+            if (!$this->waitForLock($lock_key, $full_key, $timeout)) {
+                // Clean up
+                $this->zk->remove($lock_key);
+                return null;
+            }
+
+            return $lock_key;
+        } catch (\Throwable $e) {
+            return null;
+        }
+    }
+
+    /**
+     * @param string $key
+     * @return string
+     * @throws Exception
+     */
+    private function createLockKey(string $key): string
+    {
+        if (!$this->ensurePath($key)) {
+            throw new Exception('Could not create parent node!');
+        }
+        $flags = ZkExt::EPHEMERAL | ZkExt::SEQUENCE;
+        return $this->zk->create($key, '1', $flags);
+    }
+
+    private function getLockName(string $key, string $type = 'exclusive'): string
+    {
+        switch ($type) {
+            case self::TYPE_READ:
+                $name = 'read-';
+                break;
+            case self::TYPE_WRITE:
+            case self::TYPE_EXCLUSIVE:
+            default:
+                $name = 'lock-';
+                break;
+        }
+        return $key . '/' . $name;
+    }
+
+    private function ensurePath($node): bool
+    {
+        $parent = dirname($node);
+        try {
+            if ($this->zk->exists($parent)) {
+                return true;
+            }
+            if (!$this->ensurePath($parent)) {
+                return false;
+            }
+            $this->zk->create($parent, '');
+            return true;
+        } catch (\Throwable $e) {
+            return false;
+        }
+    }
+
+    private function waitForLock(string $acquiredKey, string $baseKey, int $timeout): bool
+    {
+        $deadline = microtime(true) + $timeout;
+        $acquiredIndex = $this->getIndex($acquiredKey);
+
+        while (true) {
+            if (!$this->isAnyLock($baseKey, $acquiredIndex)) {
+                return true;
+            }
+            if ($deadline <= microtime(true)) {
+                return false;
+            }
+            usleep(100000); // sleep for a tenth of a second
+        }
+        return false;
+    }
+
+    private function getIndex(string $key): ?string
+    {
+        if (!preg_match("/[0-9]+$/", $key, $matches)) {
+            return null;
+        }
+        return intval($matches[0]);
+    }
+
+    protected function isAnyLock(string $baseKey, ?string $indexFilter = null, ?string $nameFilter = null): bool
+    {
+        $parent = dirname($baseKey);
+        if (!$this->zk->exists($parent)) {
+            return false;
+        }
+        $children = $this->zk->getChildren($parent);
+        foreach ($children as $childKey) {
+            $child = "$parent/$childKey";
+            if (!is_null($nameFilter) && strpos($child, $nameFilter) !== 0) {
+                continue;
+            }
+            if (is_null($indexFilter)) {
+                return true;
+            }
+            $child_index = $this->getIndex($childKey);
+            if (is_null($child_index)) {
+                // Not a sequence node
+                continue;
+            }
+            if ($child_index < $indexFilter) {
+                // smaller index
+                return true;
+            }
+        }
+        return false;
     }
 
 }
